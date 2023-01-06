fef94309e6232fe625ed7e00294891d1f8591a2c
NazarChykur <nazar.chykur@emburse.com>
2023-01-06 14:02:33 +0200

    Lesson 3 - Custom authentication: added CustomAuthenticationProvider

--- ss_les3/src/main/java/com/example/ss_les3/config/security/managers/CustomAuthenticationManager.java
+++ ss_les3/src/main/java/com/example/ss_les3/config/security/managers/CustomAuthenticationManager.java
@@ -1,14 +1,25 @@
 package com.example.ss_les3.config.security.managers;
 
+import com.example.ss_les3.config.security.providers.CustomAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationManager implements AuthenticationManager {
+
+    private final CustomAuthenticationProvider provider;
+    
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        if (provider.supports(authentication.getClass())) {
+            return provider.authenticate(authentication);
+        }
+        
+        throw new BadCredentialsException("Oh no ;(");
     }
 }
