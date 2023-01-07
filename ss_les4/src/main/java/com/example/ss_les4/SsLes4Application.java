4602a622c4ad9473f0f767f3a5b65b902ea8b68e
NazarChykur <nazar.chykur@emburse.com>
2023-01-07 09:35:49 +0200

    Lesson 4 - Multiple authentication providers: added schema picture and text of the flow

--- ss_les4/src/main/java/com/example/ss_les4/SsLes4Application.java
+++ ss_les4/src/main/java/com/example/ss_les4/SsLes4Application.java
@@ -11,3 +11,35 @@ public class SsLes4Application {
     }
 
 }
+
+/*
+маємо наш фільтр (CustomFilter) ApiKeyFilter, і наш AuthenticationManager, який делегує AuthenticationProvider
+цей Authentication
+
+як ми вже знаємо цепочка фільтрів йде одне за одним
+у даному випадку у нас є ApiKeyFilter і HTTP Basic фільтри, 
+    тобто перше буде відпрацьовувати наш кастомний через x-api-key у хедері,
+    якщо немає, то тоді звичайний HTTP Basic фільтр через логін і пароль
+    
+        * для AuthenticationProvider цього разу ми використали дефолтну реалізацію, 
+           але там має бути UserDetailsService + PasswordEncoder
+    
+    
+
+        CustomFilter                              DefaultFilter
+        
+            +--------------+                       +--------------+
+     -->    | ApiKeyFilter |   ---------------->   |  HTTP Basic  |  -------->  ...   
+            +--------------+                       +--------------+
+                   |                                      |
+        +-----------------------+                +-----------------------+
+        | AuthenticationManager |                | AuthenticationManager |        
+        +-----------------------+                +-----------------------+         +--------------------+
+                   |                                       |                  |--- | UserDetailsService |
+        +------------------------+              +------------------------+    |    +--------------------+
+        | AuthenticationProvider |              | AuthenticationProvider | ---|       
+        +------------------------+              +------------------------+    |    +-------------------+
+             /                                                                |--- |  PasswordEncoder  |                  
+            /                                                                      +-------------------+
+       secret Key      
+ */
