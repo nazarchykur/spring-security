f4b951dffa663011b1264adefc9a18cae2a8761a
NazarChykur <nazar.chykur@emburse.com>
2023-01-08 15:41:54 +0200

    Lesson 6 - Endpoint authorization - Part 2: .requestMatchers() => group URL pattern

--- ss_les6/src/main/java/com/example/ss_les6/config/SecurityConfig.java
+++ ss_les6/src/main/java/com/example/ss_les6/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 
         UserDetails u2 = User.withUsername("john")
                 .password(passwordEncoder().encode("pass"))
-                .authorities("write")
+                .authorities("write", "delete")
                 .build();
 
         inMemoryUserDetailsManager.createUser(u1);
@@ -42,13 +42,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                    .httpBasic()
+                .httpBasic()
                 .and()
                     .authorizeHttpRequests()
 //                        .anyRequest().authenticated()
-                        .requestMatchers("/test1").authenticated()
-                        .requestMatchers("/test2").hasAuthority("read")
+//                        .requestMatchers("/test1").authenticated()
+//                        .requestMatchers("/test2").hasAuthority("read")
 //                        .requestMatchers("/test2").permitAll()
+                        .requestMatchers("/demo/**").hasAuthority("read")
                 .and()
                 .build();
     }
@@ -81,5 +82,27 @@ public class SecurityConfig {
        
    !!! раніше використовували 
             .mvcMatchers()   - цей варіант пріоритетніший
-            .antMatchers()                     
+            .antMatchers()   
+            
+            
+    можна використовувати деякий перелік запитів   .requestMatchers("/test1", "/test3")        
+    але зручніше і практичніше описати певну групу ендпоінтів, тобто певні запити, які будуть об'єднані
+    спільним префіксом, наприклад, 
+            .requestMatchers("/demo/**")
+                і будемо знати, що /demo/test1  i /demo/test2  підпадають під цей патерн
+                
+                
+    Різниця між "/*" і "/**" полягає в тому, що другий відповідає всьому дереву каталогів, включаючи підкаталоги, 
+    a "/*" відповідає лише на рівні, на якому він указаний.
+    
+        Отже, усі ці URL-адреси відповідають тексту з шаблоном "/**".
+            /book
+            /book/20
+            /book/20/author
+            
+        URL-адреси збігаються з "/*"
+            /book
+            /magazine
+
+                              
  */
--- ss_les6/src/main/java/com/example/ss_les6/controllers/DemoController.java
+++ ss_les6/src/main/java/com/example/ss_les6/controllers/DemoController.java
@@ -1,9 +1,11 @@
 package com.example.ss_les6.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo")
 public class DemoController {
 
     @GetMapping("/test1")
