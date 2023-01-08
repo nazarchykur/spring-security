63a25132c6422e55a37ed5a904f346865c1fd302
NazarChykur <nazar.chykur@emburse.com>
2023-01-08 13:26:33 +0200

    Lesson 5 - Endpoint authorization - Part 1: .authorizeHttpRequests() + .requestMatchers()

--- ss_les5/src/main/java/com/example/ss_les5/config/SecurityConfig.java
+++ ss_les5/src/main/java/com/example/ss_les5/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.ss_les5.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,14 +20,21 @@ public class SecurityConfig {
         return http
                 .httpBasic()
                 .and()
-                    .authorizeHttpRequests()
+//                    .authorizeHttpRequests(authorize -> authorize
+//                            .requestMatchers("/hello").hasAuthority("read")
+//                            .anyRequest().authenticated())
+                .authorizeHttpRequests()
+                    .requestMatchers("/hello").hasAuthority("read")
+                    .anyRequest().authenticated()
 //                        .anyRequest().authenticated()
 //                        .anyRequest().permitAll()
 //                        .anyRequest().denyAll()
 //                .anyRequest().hasAuthority("read")
 //                .anyRequest().hasAnyAuthority("read", "write")
 //                .anyRequest().hasRole("ADMIN")
-                .anyRequest().hasAnyRole("ADMIN", "MANAGER")
+//                .anyRequest().hasAnyRole("ADMIN", "MANAGER")
+//                .mvcMatchers("/hello").hasAuthority("read")
+//                .anyRequest().authenticated()
                 .and()
                 .build();
     }
@@ -63,6 +71,13 @@ public class SecurityConfig {
 /*
     для WebApp
     
+    1) який matcher method ми повинні використовувати і як саме (anyRequest(), mvcMatchers(), antMatchers(), regexMatchers())
+    2) як додати різні authorization rules
+    
+        !!! 
+            використовувати mvcMatchers() (more secured) і краще НЕ використовувати antMatchers()
+            
+        
     
         .authorizeHttpRequests().anyRequest().authenticated()  => endpoint level authorization
         
@@ -129,5 +144,80 @@ public class SecurityConfig {
                     .hasAnyRole("ADMIN", "MANAGER")
 
 
+
+!!! 
+    загалом можна використовувати і Role і Authority разом, але краще дотримуватися чогось одного для простоти
+    і надалі буде легше дебажити
+             
+                        
+        
+        ------------------------------------------------------------------------------------------------------------------            
+        
+        Різниця між authorizeRequests і authorizeHttpRequests
+        
+        authorizeHttpRequests використовує  новий спрощений AuthorizationManagerAPI та AuthorizationFilter, 
+        тоді як authorizeRequests використовує AccessDecisionManager та FilterSecurityInterceptor. 
+    !!! Останній буде застарілим у майбутній версії Spring Security.
+        
+        
+        
+        
+   When authorizeHttpRequests is used instead of authorizeRequests, 
+   then AuthorizationFilter is used instead of FilterSecurityInterceptor.  
+   
+    1 First, the AuthorizationFilter obtains an Authentication from the SecurityContextHolder. 
+        It wraps this in an Supplier in order to delay lookup.
+    2 Second, it passes the Supplier<Authentication> and the HttpServletRequest to the AuthorizationManager.
+    3 If authorization is denied, an AccessDeniedException is thrown. In this case the ExceptionTranslationFilter 
+        handles the AccessDeniedException.
+    4 If access is granted, AuthorizationFilter continues with the FilterChain which allows the application 
+        to process normally.
+    We can configure Spring Security to have different rules by adding more rules in order of precedence.      
+    
+    
+                 
+        @Bean
+        SecurityFilterChain web(HttpSecurity http) throws Exception {
+            http
+                // ...
+                .authorizeHttpRequests(authorize -> authorize                                                1)
+                    .requestMatchers("/resources/**", "/signup", "/about").permitAll()                       2)
+                    .requestMatchers("/admin/**").hasRole("ADMIN")                                           3)
+                    .requestMatchers("/db/**").access(new WebExpressionAuthorizationManager("hasRole('ADMIN') and hasRole('DBA')"))  4)   
+                    .anyRequest().denyAll()                                                                  5)
+                );
+        
+            return http.build();
+        }   
+        
+    1) Вказано кілька правил авторизації. Кожне правило розглядається в порядку їх оголошення.
+    2) Ми вказали кілька шаблонів URL-адрес, до яких може отримати доступ будь-який користувач. Зокрема, будь-який 
+        користувач може отримати доступ до запиту, якщо URL-адреса починається з "/resources/", або "/signup", або "/about".    
+    3) Будь-яка URL-адреса, яка починається з "/admin/", буде доступна лише користувачам із роллю "ROLE_ADMIN". 
+        Зауважте, оскільки ми викликаємо метод hasRole, то не потрібно вказувати префікс "ROLE_".    
+    4) Будь-яка URL-адреса, яка починається з "/db/", вимагає від користувача одночасно "ROLE_ADMIN" і "ROLE_DBA". 
+        Зауважте, оскільки ми використовуємо hasRole вираз, нам не потрібно вказувати префікс "ROLE_".    
+    5) Доступ до будь-якої іншої URL-адреси, заборонено. 
+        Це хороша стратегія, якщо ви не хочете випадково забути оновити свої правила авторизації.     
+        
+        
+        
+        
+      
+      
+      2 варіанти:
+     
+     1) використовуємо 
+      .authorizeHttpRequests(authorize -> authorize
+         .requestMatchers("/hello").hasAuthority("read")
+         .anyRequest().authenticated())
+             
+      2)       
+        .authorizeHttpRequests()
+            .requestMatchers("/hello").hasAuthority("read")
+            .anyRequest().authenticated()  
+        
+        
+        
                         
  */
