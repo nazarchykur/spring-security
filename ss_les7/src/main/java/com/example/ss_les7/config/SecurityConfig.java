5eeafff6b17ded256406d815327c2fcb8ce58fb3
NazarChykur <nazar.chykur@emburse.com>
2023-01-08 22:41:05 +0200

    Lesson 7 - Method authorization - Part 1: @EnableMethodSecurity + @PreAuthorize

--- ss_les7/src/main/java/com/example/ss_les7/config/SecurityConfig.java
+++ ss_les7/src/main/java/com/example/ss_les7/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.ss_les7.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
--- ss_les7/src/main/java/com/example/ss_les7/controllers/DemoController.java
+++ ss_les7/src/main/java/com/example/ss_les7/controllers/DemoController.java
@@ -1,15 +1,27 @@
 package com.example.ss_les7.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/demo")
+    @GetMapping("/demo1")
+    @PreAuthorize("hasAuthority('read')")
     public String demo() {
-        return "demo";
+        return "demo1";
     }
+
+    @GetMapping("/demo2")
+    @PreAuthorize("hasAnyAuthority('read', 'write')")
+    public String demo2() {
+        return "demo2";
+    }
+    
 }
 
 
+/*
+     @PreAuthorize("hasAuthority('read')") - означає, що юзер може викликати цей ендпоінт тільки тоді якщо має права 'read'
+ */
