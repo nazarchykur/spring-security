1c41005036a8f89de233a4c7dd7ff876715805fc
NazarChykur <nazar.chykur@emburse.com>
2023-01-09 10:38:50 +0200

    Lesson 8 - Method authorization - Part 2: @PreAuthorize with SpEl

--- ss_les7/src/main/java/com/example/ss_les7/controllers/DemoController.java
+++ ss_les7/src/main/java/com/example/ss_les7/controllers/DemoController.java
@@ -2,13 +2,14 @@ package com.example.ss_les7.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
     @GetMapping("/demo1")
-    @PreAuthorize("hasAuthority('read')")
+    @PreAuthorize("hasAuthority('read')") // hasAuthority() hasAnyAuthority() hasRole() hasAnyRole()
     public String demo() {
         return "demo1";
     }
@@ -18,8 +19,20 @@ public class DemoController {
     public String demo2() {
         return "demo2";
     }
+
+    @GetMapping("/demo3/{smth}")
+    @PreAuthorize("#something == authentication.name") // authentication from SecurityContext
+    public String demo3(@PathVariable("smth") String something) {
+        return "demo3";
+    }
     
 }
+
+/*
+
+    через SpEl ми маємо доступ до параметрів і можемо брати цю змінну для перевірки  
+    authentication - це об'єкт який зберігається у SecurityContext 
+ */
 
 
 /*
